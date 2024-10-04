1228b26efb948a39f3ee189f5a9e3834069e80b2
JohhnGoblin <35317840+JohnDGodwin@users.noreply.github.com>
2024-10-04 09:54:13 -0500

    Update install-gs.sh

--- install-gs.sh
+++ install-gs.sh
@@ -10,71 +10,71 @@
 #edit /etc/ssh/sshd_config to allow for root login via ssh
 
 #sudo systemctl enable ssh
-sudo systemctl enable ssh
+systemctl enable ssh
 
 #setup scripts folder
-sudo mkdir /config/scripts
-sudo cp scripts/* /config/scripts/
+mkdir /config/scripts
+cp scripts/* /config/scripts/
 
 #setup openipc systemd service
-sudo cp openipc/openipc.service /etc/systemd/system/
-
+cp openipc/openipc.service /etc/systemd/system/
+systemctl disable openipc.service
 
 #update and upgrade system
-sudo apt update && sudo apt -y upgrade
+apt update && sudo apt -y upgrade
 
 #install prerequisite programs
-sudo apt install -y git cmake
+apt install -y git cmake
 
 #install AU driver
-sudo cp drivers/88XXau_wfb.ko /lib/modules/5.10.160-38-rk356x/kernel/drivers/net/wireless/
+cp drivers/88XXau_wfb.ko /lib/modules/5.10.160-38-rk356x/kernel/drivers/net/wireless/
 
 #install EU driver
-sudo cp drivers/8812eu.ko /lib/modules/5.10.160-38-rk356x/kernel/drivers/net/wireless/
+cp drivers/8812eu.ko /lib/modules/5.10.160-38-rk356x/kernel/drivers/net/wireless/
 
 #install wfb-ng
 git clone https://github.com/svpcom/wfb-ng.git
 cd wfb-ng
-sudo ./scripts/install_gs.sh rtl0
+./scripts/install_gs.sh rtl0
 
-sudo systemctl enable wifibroadcast
-sudo systemctl enable wifibroadcast@gs
+systemctl enable wifibroadcast
+systemctl enable wifibroadcast@gs
 
 cd ..
 
 
 #edit /etc/wifibroadcast to region 00
-sudo cp wfbng/wifibroadcast.cfg /etc/
+cp wfbng/wifibroadcast.cfg /etc/
 
 #transfer stock gs.key to /etc
-sudo cp wfbng/gs.key /etc/
+cp wfbng/gs.key /etc/
 
 
 #install PixelPilot
-sudo apt install -y librockchip-mpp-dev
-sudo apt install -y libdrm-dev libcairo-dev
-sudo apt --no-install-recommends -y install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools
+apt install -y librockchip-mpp-dev
+apt install -y libdrm-dev libcairo-dev
+apt --no-install-recommends -y install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools
 
-git clone https://github.com/OpenIPC/PixelPilot_rk.git
+clone https://github.com/OpenIPC/PixelPilot_rk.git
 cd PixelPilot_rk
 cmake -B build
-sudo cmake --build build --target install
+cmake --build build --target install
 cd ..
 
 
 #configure hotplugging of wfb-nics
 git clone https://github.com/JohnDGodwin/hot-plug-wfb-nics.git
 cd hot-plug-wfb-nics
-sudo chmod +x autoload-wfb-nics.sh
-sudo cp autoload-wfb-nics.sh /config/scripts/
-sudo cp init-nics.service /etc/systemd/system/
-sudo systemctl enable init-nics.service
-sudo cp 98-custom-wifi.rules /etc/udev/rules.d/
+chmod +x autoload-wfb-nics.sh
+cp autoload-wfb-nics.sh /config/scripts/
+cp init-nics.service /etc/systemd/system/
+systemctl enable init-nics.service
+cp 98-custom-wifi.rules /etc/udev/rules.d/
 cd ..
 
 #install the media server for dvr
-sudo apt -y install nginx-light
-sudo chmod o+x /media
-sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.old
-sudo cp nginx/default /etc/nginx/sites-available/
+apt -y install nginx-light
+chmod o+x /media
+mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.old
+cp nginx/default /etc/nginx/sites-available/
 
