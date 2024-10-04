89ec3349c1718001be9d875479f80e960f0c38c1
JohhnGoblin <35317840+JohnDGodwin@users.noreply.github.com>
2024-10-04 06:03:26 -0500

    Update install-gs.sh

--- install-gs.sh
+++ install-gs.sh
@@ -51,12 +51,7 @@ sudo cp wfbng/gs.key /etc/
 
 
 #install PixelPilot
-git clone https://github.com/rockchip-linux/mpp.git
-cd mpp
-cmake -B build
-sudo cmake --build build --target install -j4
-cd ..
-
+sudo apt install -y librockchip-mpp-dev
 sudo apt install -y libdrm-dev libcairo-dev
 sudo apt --no-install-recommends -y install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools
 
