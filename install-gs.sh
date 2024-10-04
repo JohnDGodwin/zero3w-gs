468b598e3a121d6f4c07fb826136d7e4d2beaddc
JohhnGoblin <35317840+JohnDGodwin@users.noreply.github.com>
2024-10-04 14:56:21 -0500

    Update install-gs.sh

--- install-gs.sh
+++ install-gs.sh
@@ -10,14 +10,17 @@
 #edit /etc/ssh/sshd_config to allow for root login via ssh
 
 #sudo systemctl enable ssh
+echo "systemctl enable ssh"
 systemctl enable ssh
 
 #setup scripts folder
+echo "setting up scripts folder"
 mkdir /config/scripts
-cp scripts/* /config/scripts/
+cp /zero3w-gs/scripts/* /config/scripts/
 
 #setup openipc systemd service
-cp openipc/openipc.service /etc/systemd/system/
+echo "setting up openipc systemd service"
+cp /zero3w-gs/openipc/openipc.service /etc/systemd/system/
 systemctl disable openipc.service
 
 #update and upgrade system
@@ -26,12 +29,15 @@ apt update && apt -y upgrade
 #install prerequisite programs
 apt install -y git cmake
 
+
+echo "Transferring drivers"
 #install AU driver
-cp drivers/88XXau_wfb.ko /lib/modules/5.10.160-38-rk356x/kernel/drivers/net/wireless/
+cp /zero3w-gs/drivers/88XXau_wfb.ko /lib/modules/5.10.160-38-rk356x/kernel/drivers/net/wireless/
 
 #install EU driver
-cp drivers/8812eu.ko /lib/modules/5.10.160-38-rk356x/kernel/drivers/net/wireless/
+cp /zero3w-gs/drivers/8812eu.ko /lib/modules/5.10.160-38-rk356x/kernel/drivers/net/wireless/
 
+echo "installing wfb-ng"
 #install wfb-ng
 git clone https://github.com/svpcom/wfb-ng.git
 cd wfb-ng
@@ -44,15 +50,15 @@ cd ..
 
 
 #edit /etc/wifibroadcast to region 00
-cp wfbng/wifibroadcast.cfg /etc/
+cp /zero3w-gs/wfbng/wifibroadcast.cfg /etc/
 
 #transfer stock gs.key to /etc
-cp wfbng/gs.key /etc/
+cp /zero3w-gs/wfbng/gs.key /etc/
 
 
+echo "installing PixelPilot"
 #install PixelPilot
-apt install -y librockchip-mpp-dev
-apt install -y libdrm-dev libcairo-dev
+apt install librockchip-mpp-dev libdrm-dev libcairo-dev
 apt --no-install-recommends -y install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools
 
 clone https://github.com/OpenIPC/PixelPilot_rk.git
@@ -62,9 +68,10 @@ cmake --build build --target install
 cd ..
 
 
+echo "hotplugging of wfb-nics configuration"
 #configure hotplugging of wfb-nics
 git clone https://github.com/JohnDGodwin/hot-plug-wfb-nics.git
-cd hot-plug-wfb-nics
+cd /zero3w-gs/hot-plug-wfb-nics
 chmod +x autoload-wfb-nics.sh
 cp autoload-wfb-nics.sh /config/scripts/
 cp init-nics.service /etc/systemd/system/
@@ -72,9 +79,9 @@ systemctl enable init-nics.service
 cp 98-custom-wifi.rules /etc/udev/rules.d/
 cd ..
 
+echo "installing media server for dvr"
 #install the media server for dvr
 apt -y install nginx-light
 chmod o+x /media
 mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.old
-cp nginx/default /etc/nginx/sites-available/
-
+cp /zero3w-gs/nginx/default /etc/nginx/sites-available/
