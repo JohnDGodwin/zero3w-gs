490c3b19ba9a259565f876b27a6c40ca943a8d7a
JohhnGoblin <35317840+JohnDGodwin@users.noreply.github.com>
2024-10-15 06:39:56 -0500

    Update stream.sh

--- scripts/stream.sh
+++ scripts/stream.sh
@@ -6,7 +6,7 @@ REC_FPS=$(</config/scripts/rec-fps)
 DVR_SENTINEL=$(</config/scripts/dvr-sentinel)
 
 PID=0
-BUTTON=`gpiofind PIN_27`
+BUTTON=`gpiofind PIN_32`
 
 pixelpilot --osd --osd-elements video --screen-mode $SCREEN_MODE --dvr-framerate $REC_FPS --dvr-fmp4 --dvr-template $DVR_PATH/record_$DVR_SENTINEL.mp4 &
 PID=$!
