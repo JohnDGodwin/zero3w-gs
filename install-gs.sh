a54c8222ea4d6ea842d7344735a8d1e12eec206b
JohhnGoblin <35317840+JohnDGodwin@users.noreply.github.com>
2024-10-15 03:30:24 -0500

    Update install-gs.sh

--- install-gs.sh
+++ install-gs.sh
@@ -47,6 +47,9 @@ cp /zero3w-gs/drivers/8812eu.ko /lib/modules/5.10.160-38-rk356x/kernel/drivers/n
 #install 8733bu driver
 cp /zero3w-gs/drivers/8733bu.ko /lib/modules/5.10.160-38-rk356x/kernel/drivers/net/wireless/
 
+#install atheros firmware
+apt install -y firmware-atheros
+
 
 echo "installing wfb-ng"
 #install wfb-ng
