715618122fbd1c302eff3e7826e3904ad4adae3b
JohhnGoblin <35317840+JohnDGodwin@users.noreply.github.com>
2024-12-25 04:47:15 -0600

    Update install-gs.sh

--- install-gs.sh
+++ install-gs.sh
@@ -13,15 +13,21 @@
 echo "systemctl enable ssh"
 systemctl enable ssh
 
+###
+
 #setup scripts folder
 echo "setting up scripts folder"
 mkdir /config/scripts
 cp /zero3w-gs/scripts/* /config/scripts/
 
+###
+
 #setup openipc systemd service
 echo "setting up openipc systemd service"
 cp /zero3w-gs/openipc/openipc.service /etc/systemd/system/
 systemctl disable openipc.service
+
+###
 
 #update and upgrade system
 apt update && apt -y upgrade
@@ -29,6 +35,7 @@ apt update && apt -y upgrade
 #install prerequisite programs
 apt install -y git cmake build-essential
 
+###
 
 echo "installing media server for dvr"
 #install the media server for dvr
@@ -37,11 +44,11 @@ chmod o+x /media
 mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.old
 cp /zero3w-gs/nginx/default /etc/nginx/sites-available/
 
-
+###
 
 echo "installing PixelPilot"
 #install PixelPilot
-apt install librockchip-mpp-dev libdrm-dev libcairo-dev gstreamer1.0-rockchip1 librga-dev librga2 librockchip-mpp-dev librockchip-mpp1 librockchip-vpu0 libv4l-rkmpp libgl4es libgl4es-dev
+apt install librockchip-mpp-dev libdrm-dev libcairo-dev libspdlog-dev nlohmann-json3-dev gstreamer1.0-rockchip1 librga-dev librga2 librockchip-mpp-dev librockchip-mpp1 librockchip-vpu0 libv4l-rkmpp libgl4es libgl4es-dev
 apt --no-install-recommends -y install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools
 
 git clone https://github.com/OpenIPC/PixelPilot_rk.git
@@ -50,6 +57,7 @@ cmake -B build
 cmake --build build --target install
 cd ..
 
+###
 
 echo "hotplugging of wfb-nics configuration"
 #configure hotplugging of wfb-nics
@@ -61,6 +69,8 @@ cp init-nics.service /etc/systemd/system/
 systemctl enable init-nics.service
 cp 98-custom-wifi.rules /etc/udev/rules.d/
 cd ..
+
+###
 
 echo "Transferring drivers"
 cd /zero3w-gs/drivers
@@ -82,6 +92,8 @@ cp /zero3w-gs/drivers/88x2bu.ko /lib/modules/5.10.160-299-rk356x/kernel/drivers/
 
 #install atheros firmware
 apt install -y firmware-atheros
+
+###
 
 #install wfb-ng
 echo "installing wfb-ng"
