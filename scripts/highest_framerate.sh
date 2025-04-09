3c34cb218dee797f38d52bdc639b6ccbcb41d9e4
JohhnGoblin <35317840+JohnDGodwin@users.noreply.github.com>
2025-04-09 01:32:28 -0500

    Delete scripts/highest_framerate.sh

--- scripts/highest_framerate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pixelpilot --screen-mode-list > available_screen_modes.txt
-
-awk -F'[@x]' '{print $3 " " $1 "x" $2 "@" $3}' available_screen_modes.txt | sort -k1,1nr -k2,2nr | awk '!seen[$1]++ {print $2}' > screen-mode
-sed -i '2,$d' screen-mode
-rm available_screen_modes.txt
-
-exit 0
