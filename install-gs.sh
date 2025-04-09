0337da980229f90f7d45508ce078305f527ab764
JohhnGoblin <35317840+JohnDGodwin@users.noreply.github.com>
2025-04-09 07:53:43 -0500

    Update install-gs.sh

--- install-gs.sh
+++ install-gs.sh
@@ -6,8 +6,9 @@
 
 #setting up a the image
 #change passwd for root to root
-
 #edit /etc/ssh/sshd_config to allow for root login via ssh
+
+###
 
 #sudo systemctl enable ssh
 echo "systemctl enable ssh"
@@ -36,25 +37,16 @@ apt install -y git cmake build-essential
 
 ###
 
-echo "installing media server for dvr"
-#install the media server for dvr
-apt -y install nginx-light
-chmod o+x /media
-mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.old
-cp /zero3w-gs/nginx/default /etc/nginx/sites-available/
+# echo "installing PixelPilot"
+# install PixelPilot
+# apt install librockchip-mpp-dev libdrm-dev libcairo-dev libspdlog-dev nlohmann-json3-dev gstreamer1.0-rockchip1 librga-dev librga2 librockchip-mpp-dev librockchip-mpp1 librockchip-vpu0 libv4l-rkmpp libgl4es libgl4es-dev
+# apt --no-install-recommends -y install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools
 
-###
-
-echo "installing PixelPilot"
-#install PixelPilot
-apt install librockchip-mpp-dev libdrm-dev libcairo-dev libspdlog-dev nlohmann-json3-dev gstreamer1.0-rockchip1 librga-dev librga2 librockchip-mpp-dev librockchip-mpp1 librockchip-vpu0 libv4l-rkmpp libgl4es libgl4es-dev
-apt --no-install-recommends -y install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools
-
-git clone https://github.com/OpenIPC/PixelPilot_rk.git
-cd PixelPilot_rk
-cmake -B build
-cmake --build build --target install
-cd ..
+# git clone https://github.com/OpenIPC/PixelPilot_rk.git
+# cd PixelPilot_rk
+# cmake -B build
+# cmake --build build --target install
+# cd ..
 
 ###
 
@@ -71,36 +63,36 @@ cd ..
 
 ###
 
-echo "Transferring drivers"
-cd /zero3w-gs/drivers
-dpkg -i linux-headers-5.10.160-openipc-rk356x_5.10.160-openipc_arm64.deb
-dpkg -i linux-image-5.10.160-openipc-rk356x_5.10.160-openipc_arm64.deb
-cd ..
+# echo "Transferring drivers"
+# cd /zero3w-gs/drivers
+# dpkg -i linux-headers-5.10.160-openipc-rk356x_5.10.160-openipc_arm64.deb
+# dpkg -i linux-image-5.10.160-openipc-rk356x_5.10.160-openipc_arm64.deb
+# cd ..
 
 #install EU driver
-cp /zero3w-gs/drivers/8812eu.ko /lib/modules/5.10.160-openipc-rk356x/kernel/drivers/net/wireless/
+# cp /zero3w-gs/drivers/8812eu.ko /lib/modules/5.10.160-openipc-rk356x/kernel/drivers/net/wireless/
 
 #install 8733bu driver
-cp /zero3w-gs/drivers/8733bu.ko /lib/modules/5.10.160-openipc-rk356x/kernel/drivers/net/wireless/
+# cp /zero3w-gs/drivers/8733bu.ko /lib/modules/5.10.160-openipc-rk356x/kernel/drivers/net/wireless/
 
 #install atheros firmware
-apt install -y firmware-atheros
+# apt install -y firmware-atheros
 
 ###
 
 #install wfb-ng
-echo "installing wfb-ng"
-git clone https://github.com/svpcom/wfb-ng.git
-cd wfb-ng
-./scripts/install_gs.sh rtl0
-cd ..
+# echo "installing wfb-ng"
+# git clone https://github.com/svpcom/wfb-ng.git
+# cd wfb-ng
+# ./scripts/install_gs.sh rtl0
+# cd ..
 
-systemctl enable wifibroadcast
-systemctl enable wifibroadcast@gs
+# systemctl enable wifibroadcast
+# systemctl enable wifibroadcast@gs
 
 
 #edit /etc/wifibroadcast to region 00
-cp /zero3w-gs/wfbng/wifibroadcast.cfg /etc/
+# cp /zero3w-gs/wfbng/wifibroadcast.cfg /etc/
 
 #transfer stock gs.key to /etc
-cp /zero3w-gs/wfbng/gs.key /etc/
+# cp /zero3w-gs/wfbng/gs.key /etc/
