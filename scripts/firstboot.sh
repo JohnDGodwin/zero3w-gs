105570fba3f7f5e5c2d2da4ffd214f387512bb89
JohhnGoblin <35317840+JohnDGodwin@users.noreply.github.com>
2025-05-14 16:32:13 -0500

    Create firstboot.sh

---
+++ scripts/firstboot.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+disk=$(df -P / | awk 'NR==2 {print $1}' | cut -d'/' -f3 | sed 's/[0-9]*$//' | sed 's|^|/dev/|' | sed 's/p$//')
+partition=${disk}p4
+
+
+sudo fdisk ${disk} <<EOF
+d
+
+n
+4
+
+
+t
+
+11
+p
+w
+EOF
+
+sleep 0.1
+
+
+sudo mkfs.vfat -F 32 ${partition}
+echo "UUID=$(blkid -s UUID -o value ${partition})  /dvr  vfat  defaults,umask=000  0  0" | sudo tee -a /etc/fstab
+mount ${partition}
+
+sleep 0.1
+
+sudo smbpasswd -a radxa <<EOF
+radxa
+radxa
+EOF
+
+sudo systemctl restart smbd
+
+sleep 0.1
+
+
+sudo rm /etc/systemd/system/firstboot.service
+sudo rm /config/scripts/firstboot.sh
+
+exit 0
