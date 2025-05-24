e149cf7a2944c28884f25264d0e0b22ffd7184ba
JohhnGoblin <35317840+JohnDGodwin@users.noreply.github.com>
2025-05-24 00:38:07 -0500

    Update firstboot.sh

--- scripts/firstboot.sh
+++ scripts/firstboot.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
+#Expand DVR partition
 disk=$(df -P / | awk 'NR==2 {print $1}' | cut -d'/' -f3 | sed 's/[0-9]*$//' | sed 's|^|/dev/|' | sed 's/p$//')
 partition=${disk}p4
-
 
 sudo fdisk ${disk} <<EOF
 d
@@ -20,13 +19,14 @@ EOF
 
 sleep 0.1
 
-
 sudo mkfs.vfat -F 32 ${partition}
 echo "UUID=$(blkid -s UUID -o value ${partition})  /dvr  vfat  defaults,umask=000  0  0" | sudo tee -a /etc/fstab
 mount ${partition}
 
 sleep 0.2
 
+
+# Samba stuff
 sudo smbpasswd -a radxa <<EOF
 radxa
 radxa
@@ -36,7 +36,7 @@ sudo systemctl restart smbd &
 
 sleep 0.2
 
-
+# Clean-up
 sudo rm /etc/systemd/system/firstboot.service
 sudo rm /config/scripts/firstboot.sh
 
